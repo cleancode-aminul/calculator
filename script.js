ce5cf4a0fd13ba91c481dade107ef8c0c84e75e8
clean-coder <118479101+cleancode-aminul@users.noreply.github.com>
2023-10-14 02:00:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function DeleteMe() {
+    document.getElementById("myResult").value = ""
+}
+function calculator(newValue) {
+    document.getElementById('myResult').value += newValue;
+    
+}
+function Answer() {
+    var a = document.getElementById("myResult").value;
+    var b = eval(a);
+    document.getElementById("myResult").value = b;
+}
